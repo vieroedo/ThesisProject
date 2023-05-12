0fdcd3ebe4d192cb1dc836606a0b6c1f45b727a9
Edoardo Viero <69385616+vieroedo@users.noreply.github.com>
2023-05-12 16:31:06 +0200

    plotting updates

--- FinalProblem/analytical_models_verification/equations_verification_plots.py
+++ FinalProblem/analytical_models_verification/equations_verification_plots.py
@@ -10,6 +10,13 @@ from time import process_time as pt
 from JupiterTrajectory_GlobalParameters import *
 from handle_functions import *
 
+show_orbits = True
+fontsize = 13
+# Make everything big
+plt.rc('font', size=fontsize)
+
+start_from_item_no = 4
+
 
 current_dir = os.path.dirname(__file__)
 
@@ -26,8 +33,9 @@ integer_variable_names = ['FlybyMoon']
 # * Entry 3: Flyby epoch in Julian days since J2000 (MJD2000)
 # * Entry 4: Impact parameter of the flyby in meters
 
+cmap = plt.get_cmap('viridis')
 
-number_of_runs = 10
+number_of_runs = 50
 # decision_variable_investigated =
 simulation_directory = current_dir + '/VerificationOutput'
 
@@ -62,7 +70,11 @@ for variable_no, decision_variable_investigated in enumerate(decision_variable_n
 
         fig, ax = plt.subplots()
 
+        fig_vel, ax_vel = plt.subplots()
 
+        epoch_of_minimum_altitude_range = np.array([0.,0.])
+
+        current_color = 0
 
         for run in range(number_of_runs):
             print(f'\nRun: {run}        Moon: {current_moon}')
@@ -89,7 +101,10 @@ for variable_no, decision_variable_investigated in enumerate(decision_variable_n
             cartesian_state_difference = state_difference[:,1:]
             epochs = (state_difference[:,0] - state_difference[0,0]) / constants.JULIAN_DAY
 
-            ax.plot(epochs,LA.norm(cartesian_state_difference[:,0:3], axis=1))
+            y_sam = LA.norm(cartesian_state_difference[start_from_item_no:,0:3], axis=1)/1e3
+            ax.plot(epochs[start_from_item_no:],y_sam, c=cmap(current_color))
+            ax_vel.plot(epochs[start_from_item_no:], LA.norm(cartesian_state_difference[start_from_item_no:,3:6], axis=1)/1e3, color=cmap(current_color))
+            current_color = current_color+5
 
             try:
                 numerical_orbit_state = np.loadtxt(output_path + 'simulation_state_history_' + str(run) + '.dat')
@@ -103,19 +118,55 @@ for variable_no, decision_variable_investigated in enumerate(decision_variable_n
             analytical_cartesian_orbit_state = analytical_orbit_state[:, 1:]
             x_pos_anal, y_pos_anal, z_pos_anal = analytical_cartesian_orbit_state[:, 0], analytical_cartesian_orbit_state[:,1], analytical_cartesian_orbit_state[:, 2]
 
-            # Plot 3-D Trajectory
-            fig_orbit = plt.figure()
-            ax_orbit = plt.axes(projection='3d')
+            # identify minimum altitude and epoch of occurrence
+            numerical_sim_distance_vector = LA.norm(numerical_cartesian_orbit_state[:, 0:3], axis=1)
+            minimum_altitude_cell = list(np.where(numerical_sim_distance_vector == min(numerical_sim_distance_vector))[0])
+            epoch_of_minimum_altitude = epochs[minimum_altitude_cell]
 
-            ax_orbit = plot_jupiter_and_galilean_orbits(ax_orbit, plot_orbits=False, title_addition=f'Decision variable: {decision_variable_investigated}     Value: {variable_linspace[run]}         Moon: {current_moon}')
-            ax_orbit = plot_galilean_moon(ax_orbit, current_moon, simulation_flyby_epoch)
-            ax_orbit = set_axis_limits(ax_orbit)
-            ax_orbit.plot3D(x_pos_num, y_pos_num, z_pos_num, color='gray')
-            ax_orbit.plot3D(x_pos_anal, y_pos_anal, z_pos_anal, color ='green')
+            # assign new bundaries if epoch_of_minimum_altitude is outside the given range
+            if not epoch_of_minimum_altitude_range[0] < epoch_of_minimum_altitude < epoch_of_minimum_altitude_range[1]:
+                # the first run assigns the first value to both boundaries
+                if epoch_of_minimum_altitude_range[0] == 0 and epoch_of_minimum_altitude_range[1] == 0:
+                    epoch_of_minimum_altitude_range[1] = epoch_of_minimum_altitude
+                    epoch_of_minimum_altitude_range[0] = epoch_of_minimum_altitude
+                # updates boundaries according to where the value exceeds
+                if epoch_of_minimum_altitude > epoch_of_minimum_altitude_range[1]:
+                    epoch_of_minimum_altitude_range[1] = epoch_of_minimum_altitude
+                if epoch_of_minimum_altitude < epoch_of_minimum_altitude_range[0]:
+                    epoch_of_minimum_altitude_range[0] = epoch_of_minimum_altitude
+
+            if show_orbits: # and run%5 == 0:
+                # Plot 3-D Trajectory
+                fig_orbit = plt.figure()
+                ax_orbit = plt.axes(projection='3d')
+
+                ax_orbit = plot_jupiter_and_galilean_orbits(ax_orbit, plot_orbits=False, title_addition=f'Decision variable: {decision_variable_investigated}     Value: {variable_linspace[run]}         Moon: {current_moon}')
+                ax_orbit = plot_galilean_moon(ax_orbit, current_moon, simulation_flyby_epoch)
+                ax_orbit = set_axis_limits(ax_orbit)
+                ax_orbit.plot3D(x_pos_num, y_pos_num, z_pos_num, color='gray')
+                ax_orbit.plot3D(x_pos_anal, y_pos_anal, z_pos_anal, color ='green', linestyle='--')
+
+                fig_orbit.show()
+
+        # plot vertical axis on aggregated plots that defines minimum altitude
+        ax.axvline(x=epoch_of_minimum_altitude_range[0], linestyle='--', color='grey')
+        ax.axvline(x=epoch_of_minimum_altitude_range[1], linestyle='--', color='grey')
+
+        ax_vel.axvline(x=epoch_of_minimum_altitude_range[0], linestyle='--', color='grey')
+        ax_vel.axvline(x=epoch_of_minimum_altitude_range[1], linestyle='--', color='grey')
 
 
-            fig_orbit.show()
-
+        ax = plot_grid(ax, fontsize)
+        ax_vel = plot_grid(ax_vel, fontsize)
 
         ax.set_yscale('log')
+        # ax.set_title('position error')
+        ax.set_xlabel('Elapsed time [days]')
+        ax.set_ylabel('Position error [km]')
+
+        ax_vel.set_yscale('log')
+        # ax_vel.set_title('velocity error')
+        ax_vel.set_xlabel('Elapsed time [days]')
+        ax_vel.set_ylabel('Velocity error [km/s]')
+        plt.tight_layout()
         plt.show()
